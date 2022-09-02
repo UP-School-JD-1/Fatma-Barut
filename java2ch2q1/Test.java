fab87a66bc01ec0011fc758c9106eba95d306579
fatmabarut <108763844+fatmabarut@users.noreply.github.com>
2022-09-02 21:42:06 +0300

    Add files via upload

---
+++ java2ch2q1/Test.java
@@ -0,0 +1,15 @@
+package java2ch2q1;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		   RegistrationOffice registrationOffice=new RegistrationOffice();
+		  
+		    Student student = registrationOffice.getAStudent();
+		     registrationOffice.registerStudent(student);
+		     System.out.println(student);
+
+				
+			}
+
+}
